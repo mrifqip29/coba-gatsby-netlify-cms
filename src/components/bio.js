06064a7ba3629a2f1a527554f972e0a8fab8a944
Muhammad Rifqi Prabantoro <45098478+mrifqip29@users.noreply.github.com>
2020-05-08 19:58:22 +0700

    fixing bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -57,10 +57,10 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author.name}</strong> {author.summary}
+        Ditulis oleh <strong>{author.name}</strong> {author.summary}
         {` `}
         <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
+          Silahkan follow media sosial kami!
         </a>
       </p>
     </div>
